b5cf7f20a7e93860015b1b20c3987e2569556591
Andy-Baker <1121864+David-Griffiths@users.noreply.github.com>
2019-02-16 20:01:15 +0000

    Removed wrapping li and put it into main blade template

--- resources/js/components/NovaDarkThemeToggle.vue
+++ resources/js/components/NovaDarkThemeToggle.vue
@@ -1,19 +1,17 @@
 <template>
-    <li>
-        <button
-                @click="toggle()"
-                class="block no-underline text-90 hover:bg-30 p-3 w-full text-left">
-            <icon-toggle
-                    v-if="darkThemeOn"
-                    class="w-4 text-black align-baseline ml-1 mr-4"
-                    style="margin-bottom: -2px; color: #298f5d;"></icon-toggle>
-            <icon-toggle
-                    v-else
-                    class="w-4 text-black align-baseline ml-1 mr-4"
-                    style="margin-bottom: -2px; transform: rotate(180deg); color: rgb(171, 79, 79);"></icon-toggle>
-            {{ label }}
-        </button>
-    </li>
+    <button
+            @click="toggle()"
+            class="block no-underline text-90 hover:bg-30 p-3 w-full text-left">
+        <icon-toggle
+                v-if="darkThemeOn"
+                class="w-4 text-black align-baseline ml-1 mr-4"
+                style="margin-bottom: -2px; color: #298f5d;"></icon-toggle>
+        <icon-toggle
+                v-else
+                class="w-4 text-black align-baseline ml-1 mr-4"
+                style="margin-bottom: -2px; transform: rotate(180deg); color: rgb(171, 79, 79);"></icon-toggle>
+        {{ label }}
+    </button>
 </template>
 
 <script>
